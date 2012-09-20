7704086244795bd95b04f86ca451456804951aa1
alex tsai <caffodian@gmail.com>
2012-09-20 14:18:48 -0700

    test

--- js/controllers.js
+++ js/controllers.js
@@ -10,3 +10,17 @@ MyCtrl1.$inject = [];
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+function FBItem($scope) {
+    FB.api('/220439', function(response) {
+  		alert('Your name is ' + response.name);
+  		$scope.item = response;
+	});
+  });
+
+
+ 
+  //$scope.orderProp = 'age';
+}
+
+}
